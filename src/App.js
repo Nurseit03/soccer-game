da3999f610eeb7fb49f4892ab444689609982d25
Nurseit Bakytbekov <nurseitbakytbekov@gmail.com>
2024-05-22 17:25:15 +0600

    fix borders

--- src/App.js
+++ src/App.js
@@ -9,10 +9,29 @@ const App = () => {
   const y = useMotionValue(0);
   const [goalsScore, setGoalsScore] = useState(0);
   const goalRef = useRef(null);
+  const [constraints, setConstraints] = useState({ left: 0, right: 0, top: 0, bottom: 0 });
 
   const handlePositionReset = () => {
-    controls.start({ x: 0, y: 0, transition: {duration: 0} });
+    controls.start({ x: 0, y: 0, transition: { duration: 0 } });
   };
+
+  useEffect(() => {
+    const updateConstraints = () => {
+      setConstraints({
+        left: -window.innerWidth / 2.5,
+        right: window.innerWidth / 2.5,
+        top: -window.innerHeight / 2.5,
+        bottom: window.innerHeight / 2.5,
+      });
+    };
+
+    updateConstraints();
+    window.addEventListener('resize', updateConstraints);
+
+    return () => {
+      window.removeEventListener('resize', updateConstraints);
+    };
+  }, []);
 
   useEffect(() => {
     controls.start({
@@ -45,25 +64,23 @@ const App = () => {
   }, [controls, x, y]);
 
   return (
-    <>
-        <div className="game">
-      <motion.div
-        className="ball"
-        ref={ballRef}
-        drag
-        dragConstraints={{ left: -300, right: 600, top: -300, bottom: 300 }}
-        style={{ x, y }}
-        animate={controls}
-      />
-      <div className="goal" ref={goalRef}>
-        Ворота
+    <div className="container">
+      <div className="game">
+        <motion.div
+          className="ball"
+          ref={ballRef}
+          drag
+          dragConstraints={constraints}
+          style={{ x, y }}
+          animate={controls}
+        />
+        <div className="goal" ref={goalRef}>
+        </div>
+      </div>
+      <div style={{ position: "absolute", bottom: "15px", right: "50%" }}>
+        Голов: {goalsScore}
       </div>
     </div>
-    <div style={{position: "absolute", bottom: "15px", right: "50%"}}>
-      Голов: {goalsScore}
-    </div>
-    </>
-
   );
 };
 
